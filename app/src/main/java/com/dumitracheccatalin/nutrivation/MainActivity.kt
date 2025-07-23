c9bc456ac2447eb8896948d8cb8127af55000a15
DESKTOP-39SRMK8\Sineth <dumitracheccatalin@yahoo.com>
2025-07-23 11:46:35 +0300

    Configured a sample data LazyColumn

--- app/src/main/java/com/dumitracheccatalin/nutrivation/MainActivity.kt
+++ app/src/main/java/com/dumitracheccatalin/nutrivation/MainActivity.kt
@@ -1,15 +1,25 @@
 package com.dumitracheccatalin.nutrivation
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,13 +27,18 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.dumitracheccatalin.nutrivation.ui.theme.NutrivationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MessageCard(Message("Android", "Jetpack Compose"))
+            NutrivationTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
 
         }
     }
@@ -44,18 +59,65 @@ fun MessageCard(msg: Message) {
                 .size(40.dp)
                 // Clip image to be shaped as a circle
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
         )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
         Column {
-            Text(text = msg.author)
-            Text(text = msg.body)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(all = 4.dp)
+                )
+            }
+
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+//Preview Dark Mode
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    NutrivationTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Lexi", "Hey take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
         }
     }
 }
 
 @Preview
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Lexi", "Hey take a look at Jetpack Compose, it's great!")
-    )
+fun PreviewConversation() {
+    NutrivationTheme {
+        Conversation(SampleData.conversationSample)
+    }
 }
